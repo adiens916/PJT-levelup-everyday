49414f055840449db65523dad7c0830bb5df04d8
Inho <adiens916@naver.com>
2022-09-11 14:55:31 +0900

    feat: Add habit model

--- backend/habits/models.py
+++ backend/habits/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Habit(models.Model):
+    ESTIMATE_TYPE_CHOICES = [
+        ('TIME', 'TIME'),
+        ('COUNT', 'COUNT')
+    ]
+
+    GROWTH_TYPE_CHOICES = [
+        ('INC', 'INCREASE'),
+        ('DEC', 'DECREASE')
+    ]
+
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    estimate_type = models.CharField(max_length=10, choices=ESTIMATE_TYPE_CHOICES)
+    estimate_unit = models.CharField(max_length=10)
+    final_goal = models.PositiveIntegerField()
+    growth_type = models.CharField(max_length=10, choices=GROWTH_TYPE_CHOICES)
+    day_cycle = models.PositiveSmallIntegerField(default=0)
+    
+    today_goal = models.PositiveIntegerField()
+    today_progress = models.PositiveIntegerField()
+    growth_amount = models.IntegerField()
+    last_done_date = models.DateTimeField(blank=True)
+
+    is_running = models.BooleanField(default=False)
+    is_paused = models.BooleanField(default=False)
+    start_date = models.DateTimeField(blank=True)
+    paused_date = models.DateTimeField(blank=True)
+    temporary_progress = models.PositiveIntegerField()
