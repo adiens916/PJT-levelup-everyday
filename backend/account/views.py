bc1b23c6407f8d9a918308c4a0cad65a9ac6dbd6
Inho <adiens916@naver.com>
2022-09-14 06:47:25 +0900

    feat: Change standard reset time when signing up

--- backend/account/views.py
+++ backend/account/views.py
@@ -3,7 +3,6 @@ from django.http import JsonResponse, HttpRequest, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 from .models import User
 
-    
 
 # Create your views here.
 @csrf_exempt
@@ -15,14 +14,27 @@ def signup(request: HttpRequest):
         user = User.objects.create_user(username, email, password)
         return user
 
+    def change_standard_reset_time(request: HttpRequest) -> None:
+        standard_reset_time = request.POST.get('standard_reset_time')
+        # ex) '03:30'
+        # TODO: 정규표현식으로 체크하기
+        if standard_reset_time:
+            hour, minute = standard_reset_time.split(':')
+            user.standard_reset_time = datetime.time(int(hour), int(minute))
+            user.save()
+
+
     if request.method == 'POST':
         user = create_user(request)
-        return JsonResponse({'id': user.pk})
+        change_standard_reset_time(request)
+        return JsonResponse({'id': user.pk})    
     else:
         return HttpResponseBadRequest()
 
+
 def login(request: HttpRequest):
     pass
+
 
 def logout(request: HttpRequest):
     pass
