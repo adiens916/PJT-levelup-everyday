adb887cd04c573e1d97c0ddbb1f68f351357b8ad
Inho <adiens916@naver.com>
2022-09-13 15:05:19 +0900

    feat: Add custom fields to user model

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
+# @admin.register(User)
+# class CustomUserAdmin(UserAdmin):
+#     pass
+
 # Register your models here.
-admin.site.register(User, UserAdmin)
+admin.site.register(User)
--- backend/account/models.py
+++ backend/account/models.py
@@ -1,6 +1,10 @@
+from datetime import datetime
+from time import time
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    last_reset_date = models.DateTimeField(blank=True, null=True)
+    standard_reset_time = models.TimeField(default='00:00:00')
+    is_recording = models.BooleanField(default=False)
