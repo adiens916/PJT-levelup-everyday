3ae63c151ec9a6d9f38a1e74fa9c561f4d4d703e
Inho <adiens916@naver.com>
2022-09-14 19:49:40 +0900

    feat: Make habit indexing & creating filtered by user ID

--- backend/habits/views.py
+++ backend/habits/views.py
@@ -3,7 +3,11 @@ from typing import Iterable
 
 from django.core import serializers
 from django.db.models import Model
-from django.http import HttpRequest, JsonResponse
+from django.http import (
+    JsonResponse,
+    HttpRequest, 
+    HttpResponseNotAllowed,
+)
 from django.views.decorators.csrf import csrf_exempt
 from django.utils import timezone
 
@@ -19,13 +23,18 @@ def json_response_wrapper(queryset: Iterable[Model]):
 # Create your views here.
 @csrf_exempt
 def index(request: HttpRequest):
+    if not request.user.is_authenticated:
+        result = {'success': False, 'error': 'User not authenticated'}
+        return HttpResponseNotAllowed(result, content_type='application/json')
+
     if request.method == 'GET':
-        habit_list = Habit.objects.all()
+        habit_list = Habit.objects.filter(user=request.user.pk)
         return json_response_wrapper(habit_list)
     
     elif request.method == 'POST':
         habit = Habit()
         
+        habit.user = request.user
         habit.name = request.POST.get('name')
         habit.estimate_type = request.POST.get('estimate_type')
         habit.estimate_unit = request.POST.get('estimate_unit')
