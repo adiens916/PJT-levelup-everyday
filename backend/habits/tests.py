465fce69373257de7ee31b7876fade64c66edd6a
Inho <adiens916@naver.com>
2022-09-16 08:54:34 +0900

    feat: Add day change checker

--- backend/habits/tests.py
+++ backend/habits/tests.py
@@ -1,7 +1,24 @@
-from datetime import datetime, timedelta
+from datetime import datetime, time, timedelta
 from django.test import TestCase
 
 # Create your tests here.
+def is_day_changed(date_reset_last: datetime, reset_time: time, now: datetime):
+    tomorrow = date_reset_last + timedelta(days=1) 
+    date_when_reset = datetime.combine(tomorrow.date(), reset_time)
+    return now >= date_when_reset
+
+
+def test_is_day_changed():
+    date_reset_last = datetime(2022, 9, 1, 9)
+    reset_time = time(5, 30)
+
+    date_before_reset = datetime(2022, 9, 2, hour=0)
+    assert False == is_day_changed(date_reset_last, reset_time, date_before_reset)
+
+    date_after_reset = datetime(2022, 9, 2, hour=6)
+    assert True == is_day_changed(date_reset_last, reset_time, date_after_reset)
+
+
 def is_due_today(date_done_last: datetime, day_cycle: int, now: datetime):
     delta = timedelta(day_cycle)
     return date_done_last + delta <= now
