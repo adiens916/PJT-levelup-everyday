c6fb28bcac87133e6e75b7902561e698b29381e1
Inho <adiens916@naver.com>
2022-09-16 08:43:42 +0900

    fix: Rename variables

--- backend/habits/tests.py
+++ backend/habits/tests.py
@@ -2,21 +2,25 @@ from datetime import datetime, timedelta
 from django.test import TestCase
 
 # Create your tests here.
-def is_day_changed(last_done_date: datetime, day_cycle: int, now: datetime):
+def is_due_today(date_done_last: datetime, day_cycle: int, now: datetime):
     delta = timedelta(day_cycle)
-    return last_done_date + delta <= now
+    return date_done_last + delta <= now
 
 
-def test_is_day_changed():
-    last_done_date = datetime(2022, 9, 1, 9)
+def test_is_due_today():
+    date_done_last = datetime(2022, 9, 1, 9)
     day_cycle = 1
 
     not_yet_tomorrow = datetime(year=2022, month=9, day=2, hour=8, minute=59)
-    assert False == is_day_changed(last_done_date, day_cycle, not_yet_tomorrow)
+    assert False == is_due_today(date_done_last, day_cycle, not_yet_tomorrow)
 
     tomorrow = datetime(year=2022, month=9, day=2, hour=9)
-    assert True == is_day_changed(last_done_date, day_cycle, tomorrow)
+    assert True == is_due_today(date_done_last, day_cycle, tomorrow)
 
+
+def test_is_due_today_for_long_term():
+    date_done_last = datetime(2022, 9, 1, 9)
     day_cycle = 3
-    after_a_number_of_days = datetime(year=2022, month=9, day=7, hour=9)
-    assert True == is_day_changed(last_done_date, day_cycle, after_a_number_of_days)
+
+    date_after_six_days = datetime(year=2022, month=9, day=7, hour=9)
+    assert True == is_due_today(date_done_last, day_cycle, date_after_six_days)
