df7e1180009a1f09a393216f0fa0dcf7e448b990
Inho <adiens916@naver.com>
2022-09-18 08:37:50 +0900

    feat: Add sign up template

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 // import HabitCreate from './components/HabitCreate/HabitCreate';
 // import HabitList from './components/HabitList/HabitList';
-import HabitTimer from './components/HabitTimer/HabitTimer';
+// import HabitTimer from './components/HabitTimer/HabitTimer';
+import SignUp from './components/SignUp/SignUp';
 
 function App() {
   return (
     <>
       {/* <HabitList /> */}
       {/* <HabitCreate /> */}
-      <HabitTimer />
+      {/* <HabitTimer /> */}
+      <SignUp />
     </>
   );
 }
