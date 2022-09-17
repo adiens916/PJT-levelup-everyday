dccad90cf2a0ffe11bebe672e8919286e9a85c46
Inho <adiens916@naver.com>
2022-09-17 18:15:54 +0900

    feat: Fix due date check logic & Fix tests

--- backend/habits/tests.py
+++ backend/habits/tests.py
@@ -34,29 +34,55 @@ def test_is_day_changed_when_reset_at_dawn():
     assert True == is_day_changed(reset_date, reset_hour, now)
 
 
-def is_due_today(date_done_last: datetime, day_cycle: int, now: datetime):
-    if date_done_last == None:
+def is_due_today(due_date: date, reset_hour: time, now: datetime):
+    if due_date == None:
         return False
 
-    # FIXME: 초기화 시간 고려해서 해야 함
-    days = timedelta(day_cycle)
-    return date_done_last + days <= now
+    due_date_start = datetime.combine(due_date, reset_hour)
+    due_date_end = due_date_start + timedelta(days=1)
+
+    if now < due_date_start:
+        return False
+    elif due_date_start <= now < due_date_end:
+        return True
+    elif due_date_end <= now:
+        # 원래 예정일에 접속했더라면 알아서 다음 날로 갱신이 됨.
+        # 이 경우는 예정일에 아예 접속조차 안 해서 갱신이 안 됐던 상황.
+        # 밀린 게 쌓였을 수 있으므로, 부담을 줄이기 위해 예정에서 빼놓기
+        return False
 
 
 def test_is_due_today():
-    date_done_last = datetime(2022, 9, 1, 9)
-    day_cycle = 1
+    due_date = date(2022, 9, 1)
+    reset_hour = time(6, 0)
 
-    not_yet_tomorrow = datetime(year=2022, month=9, day=2, hour=8, minute=59)
-    assert False == is_due_today(date_done_last, day_cycle, not_yet_tomorrow)
+    day_cycle = 2
+    next_due_date = due_date + timedelta(days=day_cycle)
 
-    tomorrow = datetime(year=2022, month=9, day=2, hour=9)
-    assert True == is_due_today(date_done_last, day_cycle, tomorrow)
+    now = datetime(2022, 9, 3, hour=6, minute=0)
+    assert True == is_due_today(next_due_date, reset_hour,  now)
 
 
-def test_is_due_today_for_long_term():
-    date_done_last = datetime(2022, 9, 1, 9)
-    day_cycle = 3
+def test_is_due_today_when_border_values():
+    due_date = date(2022, 9, 1)
+    reset_hour = time(6, 0)
+    
+    day_cycle = 2
+    next_due_date = due_date + timedelta(days=day_cycle)
 
-    date_after_six_days = datetime(year=2022, month=9, day=7, hour=9)
-    assert True == is_due_today(date_done_last, day_cycle, date_after_six_days)
+    now = datetime(2022, 9, 3, hour=5, minute=59)
+    assert False == is_due_today(next_due_date, reset_hour, now)
+
+    now = datetime(2022, 9, 4, hour=5, minute=59)
+    assert True == is_due_today(next_due_date, reset_hour, now)
+
+
+def test_is_due_today_when_after_due():
+    due_date = date(2022, 9, 1)
+    reset_hour = time(6, 0)
+
+    day_cycle = 2
+    next_due_date = due_date + timedelta(days=day_cycle)
+
+    now = datetime(2022, 9, 7, hour=6, minute=0)
+    assert False == is_due_today(next_due_date, reset_hour,  now)
