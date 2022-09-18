336e602cc9bdc49f44474ed838c62fb6a2ab57b1
Inho <adiens916@naver.com>
2022-09-18 15:25:04 +0900

    feat: Fix CORS policy problem

--- backend/levelup_everyday/settings.py
+++ backend/levelup_everyday/settings.py
@@ -28,6 +28,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:8000', # backend
+#     'http://localhost:3000', # frontend
+# ]
 
 # Application definition
 
@@ -38,6 +43,7 @@ INSTALLED_APPS = [
 
     # Third-party
     'rest_framework',
+    'corsheaders',
 
     # Built-in
     'django.contrib.admin',
@@ -49,6 +55,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
