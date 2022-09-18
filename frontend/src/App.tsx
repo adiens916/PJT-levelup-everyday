194706d07f050b82eb41aee5431d09705542587b
Inho <adiens916@naver.com>
2022-09-19 06:02:52 +0900

    feat: Add navigation bar template

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,16 +6,19 @@ import HabitCreate from './components/HabitCreate/HabitCreate';
 import HabitTimer from './components/HabitTimer/HabitTimer';
 import SignUp from './components/SignUp/SignUp';
 import Login from './components/Login/Login';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HabitList />} />
-        <Route path="/create" element={<HabitCreate />} />
-        <Route path="/timer" element={<HabitTimer />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
+        <Route element={<NavBar />}>
+          <Route path="/" element={<HabitList />} />
+          <Route path="/create" element={<HabitCreate />} />
+          <Route path="/timer" element={<HabitTimer />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
