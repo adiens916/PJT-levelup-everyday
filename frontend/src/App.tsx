d6ecf89c837a8ca006b39135af85b378031cf74b
Inho <adiens916@naver.com>
2022-09-19 04:42:52 +0900

    feat: Add routes for all components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
-// import HabitCreate from './components/HabitCreate/HabitCreate';
-// import HabitList from './components/HabitList/HabitList';
-// import HabitTimer from './components/HabitTimer/HabitTimer';
-// import SignUp from './components/SignUp/SignUp';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import HabitList from './components/HabitList/HabitList';
+import HabitCreate from './components/HabitCreate/HabitCreate';
+import HabitTimer from './components/HabitTimer/HabitTimer';
+import SignUp from './components/SignUp/SignUp';
 import Login from './components/Login/Login';
 
 function App() {
   return (
-    <>
-      {/* <HabitList /> */}
-      {/* <HabitCreate /> */}
-      {/* <HabitTimer /> */}
-      {/* <SignUp /> */}
-      <Login />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HabitList />} />
+        <Route path="/create" element={<HabitCreate />} />
+        <Route path="/timer" element={<HabitTimer />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
