f3aa812e61bc930ae3c2a990d2a37f93753d44f6
Inho <adiens916@naver.com>
2022-09-22 05:26:51 +0900

    feat: Add habit indexing by only one

--- backend/habits/urls.py
+++ backend/habits/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('<int:habit_id>/', views.index_each),
     path('timer/start/', views.start_timer),
     path('timer/finish/', views.finish_timer),
 ]
