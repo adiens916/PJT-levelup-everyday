89d96b9821439a38145fca563ee33beec8191409
Inho <adiens916@naver.com>
2022-09-21 12:38:07 +0900

    feat: Change menus when logging out

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,52 +1,58 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import CustomAppBar from './CustomAppBar/CustomAppBar';
 import CustomDrawer from './CustomDrawer/CustomDrawer';
-import { getUserId, logout } from '../../api/api';
+import { logout } from '../../api/api';
 
-import { useRecoilValue } from 'recoil';
+import { useRecoilState } from 'recoil';
 import { userIdState } from '../../state/state';
 
-const menusAll = [
-  {
-    name: '회원가입',
-    link: '/signup',
-    isLoginRequired: false,
-  },
-  {
-    name: '로그인',
-    link: '/login',
-    isLoginRequired: false,
-  },
-  {
-    name: '습관 목록',
-    link: '/',
-    isLoginRequired: true,
-  },
-  {
-    name: '습관 생성',
-    link: '/create',
-    isLoginRequired: true,
-  },
-  {
-    name: '진행 중인 습관',
-    link: '/timer',
-    isLoginRequired: true,
-  },
-  {
-    name: '로그아웃',
-    link: '/',
-    isLoginRequired: true,
-    onClick: logout,
-  },
-];
-
-const getFilteredMenus = (isLoggedIn: boolean) =>
-  menusAll.filter((menu) => menu.isLoginRequired === isLoggedIn);
-
 export default function NavBar() {
-  const userId = useRecoilValue(userIdState);
+  const [userId, setUserId] = useRecoilState(userIdState);
+
+  const menusAll = [
+    {
+      name: '회원가입',
+      link: '/signup',
+      isLoginRequired: false,
+    },
+    {
+      name: '로그인',
+      link: '/login',
+      isLoginRequired: false,
+    },
+    {
+      name: '습관 목록',
+      link: '/',
+      isLoginRequired: true,
+    },
+    {
+      name: '습관 생성',
+      link: '/create',
+      isLoginRequired: true,
+    },
+    {
+      name: '진행 중인 습관',
+      link: '/timer',
+      isLoginRequired: true,
+    },
+    {
+      name: '로그아웃',
+      link: '/',
+      isLoginRequired: true,
+      onClick: async () => {
+        const data = await logout();
+        if (data.success) {
+          setUserId(0);
+        }
+      },
+    },
+  ];
+
+  const getFilteredMenus = (isLoggedIn: boolean) =>
+    menusAll.filter((menu) => menu.isLoginRequired === isLoggedIn);
+
   const [menus, setMenus] = React.useState(getFilteredMenus(Boolean(userId)));
 
   const [mobileOpen, setMobileOpen] = React.useState(false);
