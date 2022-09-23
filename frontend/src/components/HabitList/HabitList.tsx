19e2f60ba15b21cc37a8e4395b83c4f31928ec57
Inho <adiens916@naver.com>
2022-09-23 21:24:25 +0900

    refactor: Use navigate hook instead of Link component

--- frontend/src/components/HabitList/HabitList.tsx
+++ frontend/src/components/HabitList/HabitList.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { Button, Container, Typography } from '@mui/material';
 
 import { getHabits } from '../../api/api';
@@ -7,6 +7,7 @@ import { HabitType } from '../../api/types';
 import { ratio } from '../../utils/utils';
 
 export default function HabitList() {
+  const navigate = useNavigate();
   const [habits, setHabits] = React.useState<HabitType[]>([]);
 
   React.useEffect(() => {
@@ -22,32 +23,30 @@ export default function HabitList() {
           {new Date().toLocaleDateString('ko')}
         </Typography>
         {habits.map((habit, index) => (
-          <Link
-            to={`/timer/${habit.id}`}
-            style={{ display: 'contents' }}
+          <Button
+            onClick={() => {
+              navigate(`/timer/${habit.id}`);
+            }}
+            variant="outlined"
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              marginY: '1rem',
+              padding: '0.5rem',
+              // background: 'linear-gradient(90deg, blue 0%, white 30%)',
+            }}
             key={index}
           >
-            <Button
-              variant="outlined"
-              sx={{
-                display: 'flex',
-                justifyContent: 'space-between',
-                marginY: '1rem',
-                padding: '0.5rem',
-                // background: 'linear-gradient(90deg, blue 0%, white 30%)',
-              }}
+            <Typography
+              // color={getRatio(habit) > 10 ? 'yellow' : 'blue'}
+              variant="h5"
             >
-              <Typography
-                // color={getRatio(habit) > 10 ? 'yellow' : 'blue'}
-                variant="h5"
-              >
-                {habit.name}
-              </Typography>
-              <Typography variant="h5">
-                {ratio(habit.today_progress, habit.today_goal)}%
-              </Typography>
-            </Button>
-          </Link>
+              {habit.name}
+            </Typography>
+            <Typography variant="h5">
+              {ratio(habit.today_progress, habit.today_goal)}%
+            </Typography>
+          </Button>
         ))}
         <Button variant="contained" sx={{ fontSize: '1.5rem' }}>
           추가
