5cea760210c3f564b7d490fc83799e5cb13f2152
Inho <adiens916@naver.com>
2022-09-26 06:26:11 +0900

    feat: Add bar chart template

--- frontend/src/components/Graph/Graph.tsx
+++ frontend/src/components/Graph/Graph.tsx
@@ -1,12 +1,62 @@
-const data = {};
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ChartData,
+  ChartOptions,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import { faker } from '@faker-js/faker';
 
-export const config = {
-  type: 'bar',
-  data,
-  options: {
-    scales: {
-      x: { stacked: true },
-      y: { beginAtZero: true },
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+);
+
+const options: ChartOptions<'bar'> = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Bar Chart',
     },
   },
+  scales: {
+    x: { stacked: true },
+    y: { beginAtZero: true },
+  },
 };
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+const data: ChartData<'bar'> = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
+export function DailyGraph() {
+  return <Bar options={options} data={data} />;
+}
