5eb1c018daa401547ce0709fe7e33f2713791425
Inho <adiens916@naver.com>
2022-09-28 14:10:01 +0900

    feat: Add hook for daily records

---
+++ frontend/src/components/Graph/useDailyRecords.ts
@@ -0,0 +1,52 @@
+import { ChartData, ChartDataset } from 'chart.js';
+import { useEffect, useState } from 'react';
+
+const initialState: DailyRecordsForChartType = {
+  labels: [],
+  goals: [],
+  progresses: [],
+  excesses: [],
+};
+
+function useDailyRecords(habitId: number) {
+  const [dailyRecords, setDailyRecords] =
+    useState<DailyRecordsForChartType>(initialState);
+
+  const datasetOfGoal: ChartDataset<'bar'> = {
+    label: 'Goal',
+    data: dailyRecords.goals,
+    backgroundColor: 'rgba(255, 255, 255, 0.5)', // White, transparent
+    borderColor: 'blue',
+    borderWidth: { top: 1, left: 1, right: 1 },
+  };
+
+  const datasetOfProgress: ChartDataset<'bar'> = {
+    label: 'Progress',
+    data: dailyRecords.progresses,
+    backgroundColor: '#00FFFF80', // Aqua
+    // backgroundColor: '#6495ED80', // CornflowerBlue
+    // backgroundColor: '#00BFFF80', // DeepSkyBlue
+  };
+
+  const datasetOfExcess: ChartDataset<'bar'> = {
+    label: 'Excess',
+    data: dailyRecords.excesses,
+    backgroundColor: '#FF149366', // DeepPink, transparent
+    borderColor: '#FF1493',
+    borderWidth: 1,
+  };
+
+  const data: ChartData<'bar'> = {
+    labels: dailyRecords.labels,
+    datasets: [datasetOfProgress, datasetOfGoal, datasetOfExcess],
+  };
+
+  return data;
+}
+
+interface DailyRecordsForChartType {
+  labels: string[];
+  goals: number[];
+  progresses: number[];
+  excesses: number[];
+}
