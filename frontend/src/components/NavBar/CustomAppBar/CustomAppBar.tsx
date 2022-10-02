f3c827a981a09955c691c41dd84be68d36fa4760
Inho <adiens916@naver.com>
2022-10-02 22:20:57 +0900

    feat: Add home button & Fix menu button to the right

--- frontend/src/components/NavBar/CustomAppBar/CustomAppBar.tsx
+++ frontend/src/components/NavBar/CustomAppBar/CustomAppBar.tsx
@@ -5,32 +5,22 @@ import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import HomeIcon from '@mui/icons-material/Home';
 import { NavBarType } from '../NavBar';
 
 export default function CustomAppBar(props: NavBarType) {
   return (
     <AppBar component="nav">
       <Toolbar>
-        <IconButton
-          color="inherit"
-          aria-label="open drawer"
-          edge="start"
-          onClick={props.handleDrawerToggle}
-          sx={{ mr: 2, display: { sm: 'none' } }}
-        >
-          <MenuIcon />
-        </IconButton>
+        <Box sx={{ flexGrow: 1 }}>
+          <Link to="/">
+            <IconButton edge="start">
+              <HomeIcon sx={{ color: 'white' }} />
+            </IconButton>
+          </Link>
+        </Box>
 
-        {/* By platform, display is changed  */}
-        <Typography
-          variant="h6"
-          component="div"
-          sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-        >
-          MUI
-        </Typography>
         <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
           {props.navItems.map((item) => (
             <Button key={item.name} sx={{ color: '#fff' }}>
@@ -47,6 +37,16 @@ export default function CustomAppBar(props: NavBarType) {
             </Button>
           ))}
         </Box>
+
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge="end"
+          onClick={props.handleDrawerToggle}
+          sx={{ display: { sm: 'none' } }}
+        >
+          <MenuIcon />
+        </IconButton>
       </Toolbar>
     </AppBar>
   );
