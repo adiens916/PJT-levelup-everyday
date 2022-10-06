fce5404b3687acef4fbc2e5412ee991b4a430cd6
Inho <adiens916@naver.com>
2022-10-06 13:02:21 +0900

    refactor: Extract unused codes into component

--- frontend/src/components/HabitList/HabitList.tsx
+++ frontend/src/components/HabitList/HabitList.tsx
@@ -20,16 +20,6 @@ export default function HabitList() {
     return formatted;
   };
 
-  const is_exist_habit_due_date = () =>
-    habits.some((habit) => habit.is_today_due_date);
-
-  // 오늘의 습관을 모두 끝마쳐야 다른 습관들을 볼 수 있음
-  const is_done_today_habits = () =>
-    habits.every(
-      (habit) =>
-        habit.is_today_due_date && habit.today_progress >= habit.today_goal,
-    );
-
   return (
     <>
       <Container
@@ -40,6 +30,7 @@ export default function HabitList() {
         <Typography textAlign="center" fontSize="1.25rem">
           {formatDateMMDD()}
         </Typography>
+
         {/* 안내 메시지 */}
         {loading ? (
           <CircularProgress sx={{ alignSelf: 'center', marginTop: '1.5rem' }} />
@@ -87,45 +78,6 @@ export default function HabitList() {
                 <HabitItem habit={habit} key={index} />
               ))}
             />
-          </>
-        )}
-        {false ? (
-          <>
-            <Typography textAlign="center" fontSize="1.5rem">
-              오늘의 습관
-            </Typography>
-            {habits.map((habit, index) => {
-              if (habit.is_today_due_date)
-                return <HabitItem habit={habit} key={index} />;
-            })}
-
-            {/* 오늘의 습관을 모두 끝내야 볼 수 있음 */}
-            {is_done_today_habits() && (
-              <>
-                <Typography
-                  textAlign="center"
-                  fontSize="1.5rem"
-                  marginTop="5rem"
-                >
-                  내일의 습관
-                </Typography>
-                {habits.map((habit, index) => {
-                  if (!habit.is_today_due_date)
-                    return <HabitItem habit={habit} key={index} />;
-                })}
-              </>
-            )}
-          </>
-        ) : (
-          <>
-            {/* 습관 목록 */}
-            {/* {habits.map((habit, index) => (
-              <HabitItem
-                habit={habit}
-                opacity={1 - 0.7 * (index / habits.length)}
-                key={index}
-              />
-            ))} */}
           </>
         )}
       </Container>
