e2fd6a715044a0b8c8c2afe96f35892ce046c0c7
Inho <adiens916@naver.com>
2022-10-11 13:44:26 +0900

    refactor: Extract error message into component

--- frontend/src/components/HabitList/HabitList.tsx
+++ frontend/src/components/HabitList/HabitList.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { CircularProgress, Container, Typography } from '@mui/material';
+import { Container, Typography } from '@mui/material';
 
 import useDocumentTitle from '../../hook/useDocumentTitle';
 import useHabitList from './useHabitList';
+import ErrorGuide from './ErrorGuide/ErrorGuide';
 import HabitListContainerGroup from './HabitListContainerGroup/HabitListContainerGroup';
 import { formatDateMmDd } from '../../utils/utils';
 
@@ -23,23 +23,12 @@ export default function HabitList() {
         </Typography>
 
         {/* 안내 메시지 */}
-        {loading ? (
-          <CircularProgress sx={{ alignSelf: 'center', marginTop: '1.5rem' }} />
-        ) : errorCode === 403 || (!isError && habits && habits.length === 0) ? (
-          <Typography textAlign="center" marginTop="2rem">
-            <Link to="/create">새로운 습관</Link>을 만들어봅시다 ٩(ˊᗜˋ*)و
-          </Typography>
-        ) : errorCode === 401 ? (
-          <Typography textAlign="center" marginTop="2rem">
-            <Link to="/login">로그인</Link>을 해주세요 (・-・)
-          </Typography>
-        ) : (
-          isError && (
-            <Typography textAlign="center" marginTop="2rem">
-              서버 연결 불가 (´；ω；｀)
-            </Typography>
-          )
-        )}
+        <ErrorGuide
+          habits={habits}
+          loading={loading}
+          isError={isError}
+          errorCode={errorCode}
+        />
 
         {/* 상태별로 정렬 */}
         {!loading && habits && habits.length && (
