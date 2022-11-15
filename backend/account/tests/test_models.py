b61312d717d4d720ef40cbb61d0164b17be7f506
Inho <adiens916@naver.com>
2022-11-15 14:07:45 +0900

    test: Move method

--- backend/account/tests/test_models.py
+++ backend/account/tests/test_models.py
@@ -22,12 +22,18 @@ class UserModelTestCase(TestCase):
         mocked_datetime.now.return_value = datetime(2022, 9, 1, hour=22, minute=30)
         self.assertIs(self.user.is_day_changed(), True)
 
-    # def test_is_day_changed_when_reset_at_dawn():
-    #     reset_date = date(2022, 9, 17)
-    #     reset_hour = time(6, 0)
+    @mock.patch("account.models.datetime", wraps=datetime)
+    def test_is_day_changed_when_reset_at_dawn(self, mocked_datetime):
+        # given: a user's reset time is AM 06:00
+        self.user.next_reset_date = date(2022, 9, 17)
+        self.user.daily_reset_time = time(6, 0)
 
-    #     now = datetime(2022, 9, 17, hour=0)
-    #     assert False == is_day_changed(reset_date, reset_hour, now)
+        # when: now is AM 00:00
+        mocked_datetime.now.return_value = datetime(2022, 9, 17, hour=0)
+        # then: not changed yet
+        self.assertIs(self.user.is_day_changed(), False)
 
-    #     now = datetime(2022, 9, 17, hour=7)
-    #     assert True == is_day_changed(reset_date, reset_hour, now)
+        # when: now is AM 07:00
+        mocked_datetime.now.return_value = datetime(2022, 9, 17, hour=7)
+        # then: changed
+        self.assertIs(self.user.is_day_changed(), True)
