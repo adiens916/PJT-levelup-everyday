709d2c8fe39c435fe631b12ac5b21dc765694ded
Inho <adiens916@naver.com>
2022-11-19 22:34:20 +0900

    test: Add view test class

---
+++ backend/habits/tests/test_views.py
@@ -0,0 +1,14 @@
+from unittest import mock
+
+from django.test import TestCase
+
+from account.models import User
+from habits.models import Habit
+
+
+class HabitViewTestCase(TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def test_create_habit(self):
+        pass
