31ea37fd98d918b2e7e5fa759a88c663d40efa17
Inho <adiens916@naver.com>
2022-11-19 22:31:22 +0900

    test: Add test for checking auth

--- backend/account/tests/test_views.py
+++ backend/account/tests/test_views.py
@@ -29,3 +29,21 @@ class AccountViewTestCase(TestCase):
         self.assertIsNone(items.get("token"))
         self.assertIsNone(items.get("user_id"))
         self.assertFalse(items.get("success"))
+
+    def test_check_authenticated(self):
+        credentials = {"username": "john", "password": "doe"}
+        response = self.client.post("/api/account/login/", credentials)
+
+        items: dict = response.json()
+        token = items.get("token")
+        print("token: ", token)
+
+        # NOTE: Headers should be in upper case (capital letters).
+        headers = {"HTTP_AUTHORIZATION": f"Token {token}"}
+        response = self.client.post("/api/account/check/", **headers)
+        # print(response.content)
+        # print("< response >")
+        # print(*response.items(), sep="\n")
+
+        self.assertContains(response, "id")
+        self.assertContains(response, "name")
