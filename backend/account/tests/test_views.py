683fde5010748da381faf6df061d474d1f97f809
Inho <adiens916@naver.com>
2022-11-19 16:45:52 +0900

    test: Add test for login failed

--- backend/account/tests/test_views.py
+++ backend/account/tests/test_views.py
@@ -18,3 +18,13 @@ class AccountViewTestCase(TestCase):
         self.assertTrue(items.get("token"))
         self.assertTrue(items.get("user_id"))
         self.assertIsNone(items.get("success"))
+
+    def test_login_failed(self):
+        credentials = {"username": "john", "password": "not_password"}
+        response = self.client.post("/api/account/login/", credentials)
+        # print(response.client)
+
+        items: dict = response.json()
+        self.assertIsNone(items.get("token"))
+        self.assertIsNone(items.get("user_id"))
+        self.assertFalse(items.get("success"))
