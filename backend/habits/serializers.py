295b37f611e7e5875c64b665e9d1890d15c743d0
Inho <adiens916@naver.com>
2022-12-19 12:04:01 +0900

    [feat] Add serializer

--- backend/habits/serializers.py
+++ backend/habits/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
-from models import *
+from .models import *
+
 
 class HabitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Habit
-        fields = '__all__'
+        fields = "__all__"
+
+
+class RoundRecordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RoundRecord
+        fields = "__all__"
+
+
+class DailyRecordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DailyRecord
+        fields = "__all__"
