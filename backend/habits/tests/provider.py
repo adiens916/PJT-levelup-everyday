502d868ad1d3b15745cc89e034f8a846508954f6
Inho <adiens916@naver.com>
2022-12-24 18:29:53 +0900

    [test] Add test data & methods to test

--- backend/habits/tests/provider.py
+++ backend/habits/tests/provider.py
@@ -3,6 +3,7 @@ from account.models import User
 
 USERNAME = "john"
 PASSWORD = "doe"
+
 HABIT_INFO = {
     "name": "Reading a book",
     "estimate_type": "TIME",
@@ -12,19 +13,42 @@ HABIT_INFO = {
     "day_cycle": 2,
     "initial_goal": 300,
 }
+HABIT_INFO_2 = {
+    "name": "Exercise",
+    "estimate_type": "TIME",
+    "estimate_unit": "",
+    "final_goal": 1800,
+    "growth_type": "INCREASE",
+    "day_cycle": 3,
+    "initial_goal": "",
+}
+HABIT_INFO_3 = {
+    "name": "Study",
+    "estimate_type": "TIME",
+    "estimate_unit": "",
+    "final_goal": 2700,
+    "growth_type": "INCREASE",
+    "day_cycle": 1,
+    "initial_goal": "",
+}
 
 
 class TestDataProvider:
     def __init__(self) -> None:
         self.user = User.objects.create_user(username=USERNAME, password=PASSWORD)
+        self.__auth_headers = None
 
     def get_auth_headers(self) -> dict:
-        credentials = {"username": USERNAME, "password": PASSWORD}
-        response = Client().post("/api/account/login/", credentials)
+        if not self.__auth_headers:
+            credentials = {"username": USERNAME, "password": PASSWORD}
+            response = Client().post("/api/account/login/", credentials)
 
-        items: dict = response.json()
-        token = items.get("token")
-        return {"HTTP_AUTHORIZATION": f"Token {token}"}
+            items: dict = response.json()
+            token = items.get("token")
+
+            self.__auth_headers = {"HTTP_AUTHORIZATION": f"Token {token}"}
+
+        return self.__auth_headers
 
     def create_habit(self) -> int:
         response = Client().post(
@@ -34,3 +58,7 @@ class TestDataProvider:
         items: dict = response.json()
         habit_id = items.get("id")
         return habit_id
+
+    def create_habits(self) -> None:
+        for habit_info in (HABIT_INFO_2, HABIT_INFO_3):
+            Client().post("/api/habit/", data=habit_info, **self.get_auth_headers())
--- backend/habits/tests/test_views.py
+++ backend/habits/tests/test_views.py
@@ -10,9 +10,25 @@ class HabitViewTestCase(TestCase):
         provider = TestDataProvider()
         cls.auth_headers = provider.get_auth_headers()
         cls.habit_id = provider.create_habit()
+        provider.create_habits()
 
     def test_create_habit(self):
         habit = Habit.objects.get(pk=self.habit_id)
         self.assertEqual(habit.name, "Reading a book")
         self.assertEqual(habit.final_goal, 3600)
         self.assertEqual(habit.goal_xp, 300)
+
+    def test_get_habits(self):
+        pass
+
+    def test_get_a_habit(self):
+        pass
+
+    def test_delete_habit(self):
+        pass
+
+    def test_update_importance(self):
+        pass
+
+    def test_get_daily_records(self):
+        pass
