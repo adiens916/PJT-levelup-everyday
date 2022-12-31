1df48d5b8fda0eed8dcb236423259a20d5a9e943
Inho <adiens916@naver.com>
2022-12-31 10:35:08 +0900

    [refactor] Reorder methods

--- frontend/src/domain/account/api.test.ts
+++ frontend/src/domain/account/api.test.ts
@@ -1,12 +1,26 @@
 import {
-  login,
-  getUserToken,
   checkConnection,
   checkPostAvailability,
+  getUserToken,
+  login,
 } from './api';
 
-describe('login tests', () => {
-  test.skip('login', async () => {
+describe.skip('connection tests', () => {
+  // If you want to test these, remove '.skip' in 'describe.skip'
+  // Ex) describe('connection tests', ... )
+  test('check connection', async () => {
+    const data = await checkConnection();
+    expect(data).toBe('connected');
+  });
+
+  test('check post availability', async () => {
+    const data = await checkPostAvailability();
+    expect(data).toBe('post available');
+  });
+});
+
+describe.skip('login tests', () => {
+  test('login', async () => {
     try {
       const result = await login('john', 'johnjohn');
       expect(result.token).toMatch(/[0-9a-z]{40}/);
@@ -20,19 +34,5 @@ describe('login tests', () => {
     await login('john', 'johnjohn');
     expect(getUserToken()).toMatch(/[0-9a-z]{40}/);
     // result.token == c59705415cde035735c37c4711d97f56c49648b6
-  });
-});
-
-describe.skip('connection tests', () => {
-  // If you want to test these, remove '.skip' in 'describe.skip'
-  // Ex) describe('connection tests', ... )
-  test('check connection', async () => {
-    const data = await checkConnection();
-    expect(data).toBe('connected');
-  });
-
-  test('check post availability', async () => {
-    const data = await checkPostAvailability();
-    expect(data).toBe('post available');
   });
 });
