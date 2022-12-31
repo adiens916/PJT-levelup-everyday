eec82c12f65d4be572a87a61b3bde060b263a2fc
Inho <adiens916@naver.com>
2022-12-31 10:03:34 +0900

    [refactor] Group tests

--- frontend/src/domain/account/api.test.ts
+++ frontend/src/domain/account/api.test.ts
@@ -1,21 +1,27 @@
 import { checkConnection, checkPostAvailability, login } from './api';
 
-test('login', async () => {
-  try {
-    const result = await login('john', 'johnjohn');
-    expect(result.token).toMatch(/[0-9a-z]{40}/);
-    // result.token == c59705415cde035735c37c4711d97f56c49648b6
-  } catch (error) {
-    console.log(error);
-  }
+describe('login tests', () => {
+  test('login', async () => {
+    try {
+      const result = await login('john', 'johnjohn');
+      expect(result.token).toMatch(/[0-9a-z]{40}/);
+      // result.token == c59705415cde035735c37c4711d97f56c49648b6
+    } catch (error) {
+      console.log(error);
+    }
+  });
 });
 
-test('check connection', async () => {
-  const data = await checkConnection();
-  expect(data).toBe('connected');
-});
+describe.skip('connection tests', () => {
+  // If you want to test these, remove '.skip' in 'describe.skip'
+  // Ex) describe('connection tests', ... )
+  test('check connection', async () => {
+    const data = await checkConnection();
+    expect(data).toBe('connected');
+  });
 
-test('check post availability', async () => {
-  const data = await checkPostAvailability();
-  expect(data).toBe('post available');
+  test('check post availability', async () => {
+    const data = await checkPostAvailability();
+    expect(data).toBe('post available');
+  });
 });
