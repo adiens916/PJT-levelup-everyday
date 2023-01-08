73247cc12f5d9e3ba1d9d43d1ffb8436036ec5eb
Inho <adiens916@naver.com>
2023-01-08 18:49:24 +0900

    [feat] Add relative date extractor

--- backend/account/models_aux.py
+++ backend/account/models_aux.py
@@ -1,11 +1,32 @@
 import re
+from datetime import date, datetime, time, timedelta
 
 
 class DateTimeCalculator:
+    def __init__(self, date: date, time=time(0, 0)) -> None:
+        self.__date = date
+        self.__time = time
+        self.datetime = datetime.combine(self.__date, self.__time)
+
+    def is_day_passed(self) -> bool:
+        return self.datetime <= datetime.now()
+
     @staticmethod
-    def is_iso_format(time: str):
+    def get_relative_date(absolute_datetime: datetime, reference_time: time) -> date:
+        hours = reference_time.hour
+        minutes = reference_time.minute
+        offset = timedelta(hours=hours, minutes=minutes)
+
+        relative_datetime = absolute_datetime - offset
+        if reference_time.hour < 12:
+            return relative_datetime.date()
+        else:
+            return relative_datetime.date() + timedelta(days=1)
+
+    @staticmethod
+    def is_iso_format(time: str) -> bool:
         iso_format = "([0-1][0-9]|2[0-3]):([0-5][0-9])"
         # Ex.) '03:30'
 
         matched = re.match(iso_format, time)
-        return matched
+        return matched != None
--- backend/account/tests/test_models_aux.py
+++ backend/account/tests/test_models_aux.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from datetime import date, datetime, time
 
 from account.models_aux import DateTimeCalculator
 
@@ -7,8 +8,58 @@ class DateTimeCalculatorTestCase(TestCase):
     def test_is_iso_format(self):
         given_time = "03:30"
         matched = DateTimeCalculator.is_iso_format(given_time)
-        self.assertIsNotNone(matched)
+        self.assertTrue(matched)
 
         wrong_time = "3:30"
         matched = DateTimeCalculator.is_iso_format(wrong_time)
-        self.assertIsNone(matched)
+        self.assertFalse(matched)
+
+    def test_get_relative_date_for_before_midnight(self):
+        reset_time = time(22, 0)
+
+        now = datetime(2023, 1, 8, 20, 0)
+        relative_date = DateTimeCalculator.get_relative_date(now, reset_time)
+        self.assertEqual(relative_date, date(2023, 1, 8), "2023-01-08 20:00")
+
+        now = datetime(2023, 1, 8, 22, 0)
+        relative_date = DateTimeCalculator.get_relative_date(now, reset_time)
+        self.assertEqual(relative_date, date(2023, 1, 9), "2023-01-08 22:00")
+
+        now = datetime(2023, 1, 8, 23, 0)
+        relative_date = DateTimeCalculator.get_relative_date(now, reset_time)
+        self.assertEqual(relative_date, date(2023, 1, 9), "2023-01-08 23:00")
+
+        now = datetime(2023, 1, 9, 2, 0)
+        relative_date = DateTimeCalculator.get_relative_date(now, reset_time)
+        self.assertEqual(relative_date, date(2023, 1, 9), "2023-01-09 02:00")
+
+        now = datetime(2023, 1, 9, 7, 0)
+        relative_date = DateTimeCalculator.get_relative_date(now, reset_time)
+        self.assertEqual(relative_date, date(2023, 1, 9), "2023-01-09 07:00")
+
+        now = datetime(2023, 1, 9, 13, 0)
+        relative_date = DateTimeCalculator.get_relative_date(now, reset_time)
+        self.assertEqual(relative_date, date(2023, 1, 9), "2023-01-09 13:00")
+
+    def test_get_relative_date_for_after_midnight(self):
+        reset_time = time(2, 0)
+
+        now = datetime(2023, 1, 8, 23, 0)
+        relative_date = DateTimeCalculator.get_relative_date(now, reset_time)
+        self.assertEqual(relative_date, date(2023, 1, 8))
+
+        now = datetime(2023, 1, 9, 1, 0)
+        relative_date = DateTimeCalculator.get_relative_date(now, reset_time)
+        self.assertEqual(relative_date, date(2023, 1, 8))
+
+        now = datetime(2023, 1, 9, 2, 0)
+        relative_date = DateTimeCalculator.get_relative_date(now, reset_time)
+        self.assertEqual(relative_date, date(2023, 1, 9))
+
+        now = datetime(2023, 1, 9, 3, 0)
+        relative_date = DateTimeCalculator.get_relative_date(now, reset_time)
+        self.assertEqual(relative_date, date(2023, 1, 9))
+
+        now = datetime(2023, 1, 9, 14, 0)
+        relative_date = DateTimeCalculator.get_relative_date(now, reset_time)
+        self.assertEqual(relative_date, date(2023, 1, 9))
